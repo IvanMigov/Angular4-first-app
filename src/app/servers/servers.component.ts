eb2f0739887116a0c3711a6635b97ecb2129e7f6
Ivan Migov <ivan@revimedia.com>
2017-04-25 17:32:09 +0300

    before ht 37

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,9 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = "No server was created";
   serverName = 'start value';
+  serverCreated = false;
+  servers = ['TestServer','RestServer 2'];
+
   constructor() {
     setTimeout(()=>{
       this.allowNewServer = true;
@@ -18,6 +21,8 @@ export class ServersComponent implements OnInit {
   ngOnInit() {}
   onCreateServer() {
     this.serverCreationStatus = 'server was created '+ this.serverName;
+    this.servers.push(this.serverName);
+    this.serverCreated = true;
   };
   onUpdateServerName(event: any){
     this.serverName = (<HTMLInputElement>event.target).value;
